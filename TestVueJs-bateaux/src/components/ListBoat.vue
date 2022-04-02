4f20366e11db3c47060d18359bfd45df8a1fb171
NoodMan <camara.adeline@yahoo.fr>
2022-04-02 20:15:27 +0200

    pffff

--- TestVueJs-bateaux/src/components/ListBoat.vue
+++ TestVueJs-bateaux/src/components/ListBoat.vue
@@ -19,22 +19,40 @@
         </tr>
 
         <tbody>
-          <tr>
-            <td>{{ store.name }}</td>
-            <td>{{ store.owner }}</td>
-            <td class="table-active">{{ store.profitability }}</td>
+          <tr v-for="g_b in profitability" :key="g_b">>
+            <td>{{ boat.name }}</td>
+            <td>{{ boat.owner }}</td>
+            <td class="table-active">{{ boat.profitability }}</td>
           </tr>
         </tbody>
       </table>
       <br />
+      <ListBoat />
     </div>
   </center>
  
 </template>
 
-<script setup>
+<script>
 //pour pouvoir utiliser les données du store
-import { useBoatsStore } from "../stores/storeboat"
+// import { useBoatsStore } from "../stores/storeboat"
 //et ne pas oublierle const
-const store = useBoatsStore();
+// const store = useBoatsStore();
+// const g_b = getBoats();
+
+
+import { mapWritableState } from "pinia";
+import { useBoatsStore } from "../stores/storeboat"
+
+export default {
+  computed: {
+    ...mapWritableState(useBoatsStore, {
+      profitability: "getBoats",
+     
+    }),
+  },
+};
+
+
+
 </script>
